0f9fd87bd3f151d9227891fab89a4d26320c5c0c
OpenKollellaullin <cyslabs@gmail.com>
2016-09-01 07:26:58 -0300

    Create aritmetica.py

---
+++ aritmetica.py
@@ -0,0 +1,27 @@
+# SE DEFINEN DOS NUMEROS
+a=7
+b= 3
+
+#SUMAR
+resultado = a + b
+print resultado
+
+#RESTAR
+resultado = a - b
+print resultado
+
+#MULTIPLICAR
+resultado = a * b
+print resultado
+
+#DIVIDIR
+resultado = a / b 
+print resultado
+
+#MODULO O RESIDUO
+resultado = a % b 
+print resultado
+
+#EXPONENCIAL
+resultado = a ** b
+print resultado
